9a2d018f51bbdd49c72bdd16b92d1df1baa73ac7
Jesús Malo Poyatos <jmalo@gsumalo.com>
2016-03-17 21:54:12 +0100

    Adding initial parser for command line options

--- gsuite.cpp
+++ gsuite.cpp
@@ -1,15 +1,30 @@
 #include <cstdlib>
-
+#include <iostream>
+#include <boost/program_options.hpp>
 #include "kernels/io/naive_std_writer.hpp"
 
 int main(int argc, const char * argv[])
 {
     int rv(EXIT_SUCCESS);
 
-    try {
-        kernels::io::NaiveStdWriter primer;
+    const std::string HELP_COMMAND{"help"};
 
-        primer.run();
+    try {
+        boost::program_options::options_description commands_spec("Allowed options");
+        commands_spec.add_options()
+                (HELP_COMMAND.c_str(), "Show this help");
+
+        boost::program_options::variables_map vm;
+        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, commands_spec), vm);
+        boost::program_options::notify(vm);
+
+        if (vm.count(HELP_COMMAND)) {
+            std::cout << commands_spec << std::endl;
+        } else {
+            kernels::io::NaiveStdWriter primer;
+
+            primer.run();
+        }
     } catch (...) {
         rv = EXIT_FAILURE;
     }
