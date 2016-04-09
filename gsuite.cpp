08ea0e9b8e88a811b0931325d6c795006f9faad4
Jesús Malo Poyatos <jmalo@gsumalo.com>
2016-04-09 22:36:41 +0200

    Adding time measuring

--- gsuite.cpp
+++ gsuite.cpp
@@ -1,11 +1,13 @@
+#include <chrono>
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
 #include <boost/program_options.hpp>
 #include "kernels/io/naive_std_writer.hpp"
 
 int main(int argc, const char * argv[])
 {
-    int rv(EXIT_SUCCESS);
+    int rv{EXIT_SUCCESS};
 
     const std::string HELP_COMMAND{"help"};
     const std::string KERNEL_COMMAND{"kernel"};
@@ -14,17 +16,17 @@ int main(int argc, const char * argv[])
     const std::string KERNEL_IO_NAIVESTDWRITER_BUFFERSIZE_OPTION{"io.naivestdwriter.buffersize"};
 
     try {
-        boost::program_options::options_description commands_spec("Allowed options");
+        boost::program_options::options_description commands_spec{"Allowed options"};
 
         std::string kernel_selection;
-        boost::program_options::options_description generic_spec("Generic commands");
+        boost::program_options::options_description generic_spec{"Generic commands"};
         generic_spec.add_options()
                 (HELP_COMMAND.c_str(), "Show this help")
                 (KERNEL_COMMAND.c_str(), boost::program_options::value<std::string>(&kernel_selection),
                         ("Kernel selection (" + KERNEL_IO_NAIVESTDWRITER_SELECTION + ")").c_str());
 
         uint64_t size, buffer_size;
-        boost::program_options::options_description kernels_spec("Available kernels");
+        boost::program_options::options_description kernels_spec{"Available kernels"};
         kernels_spec.add_options()
                 (KERNEL_IO_NAIVESTDWRITER_SIZE_OPTION.c_str(),
                         boost::program_options::value<uint64_t>(&size)->default_value(0), "Size of the generated file");
@@ -44,9 +46,22 @@ int main(int argc, const char * argv[])
             if (kernel_selection == KERNEL_IO_NAIVESTDWRITER_SELECTION) {
                 if (vm.count(KERNEL_IO_NAIVESTDWRITER_SIZE_OPTION)
                         && vm.count(KERNEL_IO_NAIVESTDWRITER_BUFFERSIZE_OPTION)) {
-                    kernels::io::NaiveStdWriter primer(size, buffer_size);
+                    kernels::io::NaiveStdWriter primer{size, buffer_size};
 
+                    const auto start_time(std::chrono::high_resolution_clock::now());
                     primer.run();
+                    const auto stop_time(std::chrono::high_resolution_clock::now());
+                    const auto elapsed_time(stop_time - start_time);
+
+
+                    // split into hours, minutes, seconds, and milliseconds
+                    const auto hh(std::chrono::duration_cast<std::chrono::hours>(elapsed_time));
+                    const auto mm(std::chrono::duration_cast<std::chrono::minutes>(elapsed_time % std::chrono::hours(1)));
+                    const auto ss(std::chrono::duration_cast<std::chrono::seconds>(elapsed_time % std::chrono::minutes(1)));
+                    const auto msec(std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_time % std::chrono::seconds(1)));
+
+                    std::cout << std::setfill('0') << std::setw(2) << hh.count() << ":" << std::setw(2) << mm.count()
+                            << ":" << std::setw(2) << ss.count() << "." << std::setw(3) << msec.count() << std::endl;
                 } else {
                     throw std::runtime_error("Missing arguments");
                 }
